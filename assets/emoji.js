fe5f56c0b542e2f0d77e1dfee5d0e4a98ffd2fed
gunhachan <66863290+gunhachan@users.noreply.github.com>
2025-08-08 00:17:31 +0530

    Forgot emojis, added those

---
+++ assets/emoji.js
@@ -0,0 +1,17 @@
+// 💡 You can change emojis here
+export const emojiMap = {
+    "Art Exhibition": "🎨",
+    "Dance": "💃🏻",
+    "Film Screening": "🎬",
+    "Fitness/Wellness": "💪",
+    "Food/Drink": "🍕",
+    "Literature": "📚",
+    "Market/Popup": "🛒",
+    "Meetup/Social": "👥",
+    "Music": "🎵",
+    "Other": "🌀",
+    "Quiz": "❓",
+    "Talk": "🎤",
+    "Theatre": "🎭",
+    "Workshop": "🧠"
+};
